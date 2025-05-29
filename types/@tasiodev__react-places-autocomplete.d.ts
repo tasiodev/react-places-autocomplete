59381c5dda305669dbaf2d9a8adce1c7c1352560
tasio <39553179+tasiodev@users.noreply.github.com>
2025-05-29 14:00:18 +0000

    ts support and add react 19 peer

---
+++ types/@tasiodev__react-places-autocomplete.d.ts
@@ -0,0 +1,5 @@
+declare module '@tasiodev/react-places-autocomplete' {
+  import * as React from 'react';
+  const PlacesAutocomplete: React.ComponentType<any>;
+  export default PlacesAutocomplete;
+}
